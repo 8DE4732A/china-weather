e98681903147059414a94cfd204b3367a39b0bfa
pikachu <liuping@zhongan.com>
2022-11-22 18:29:42 +0800

    ffmpeg

--- ln.py
+++ ln.py
@@ -1,8 +1,9 @@
+import sys
 from pathlib import Path
 
-p = Path('D:\weather')
+p = Path(sys.path[0] + '/weather')
 file_names = [x.name for x in p.iterdir()]
 nums = sorted([x.split('.')[0] for x in file_names])
 
 for i in range(len(nums)):
-    Path('D:\weather-ffmpeg\\' + '{0:06d}'.format(i) + '.jpg').symlink_to('D:\weather\\' + nums[i] + '.jpg')
+    Path(sys.path[0] + '/weather-ffmpeg/' + '{0:06d}'.format(i) + '.jpg').symlink_to(sys.path[0] + '/weather/' + nums[i] + '.jpg')
