7ab42aed969d594ca112de9068b253748cf809c4
pikachu <liuping@zhongan.com>
2022-11-30 15:26:34 +0800

    timeout

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ req = urllib.request.Request("http://d1.weather.com.cn/satellite2015/JC_YT_DL_WX
 req.add_header('Referer', 'http://www.weather.com.cn/satellite/')
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36')
 
-with urllib.request.urlopen(req) as f:
+with urllib.request.urlopen(req, timeout=60) as f:
     data = f.read().decode('utf-8')
     data = data[14:-1]
     j = json.loads(data.replace('\'','\"'))
